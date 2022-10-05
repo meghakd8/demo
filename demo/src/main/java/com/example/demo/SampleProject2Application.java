571221ea4c98696e64b15a7b707a4548d5460780
meghakd8 <meghakd8@gmail.com>
2022-10-05 20:57:36 +0530

    Initial commit

---
+++ demo/src/main/java/com/example/demo/SampleProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProject2Application.class, args);
+	}
+
+}
